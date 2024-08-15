762565abcc1e010b81182c250b28112be5442211
Pavlo Yurchenko <pashtet.gm@gmail.com>
2024-08-15 12:04:12 +0300

    Features and GetApp sections + button component fix

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,15 +5,17 @@ type ButtonProps = {
   title: string;
   icon?: string;
   variant: string;
+  full?: boolean;
 };
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
   return (
     <button
-      className={`flexCenter gap-3 rounded-full border-none ${variant}`}
+      className={`flexCenter gap-3 rounded-full ${variant} 
+      ${full && "w-full"}`}
       type={type}
     >
       {icon && <Image src={icon} alt={title} width={24} height={24} />}
-      <span className="bold-16 whitespace-nowrap">{title}</span>
+      <span className="bold-16 whitespace-nowrap cursor-pointer">{title}</span>
     </button>
   );
 };
